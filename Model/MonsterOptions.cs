375f63ca6766f79a1d83c3757253b3805b2bb5c4
Chris Rancourt <rancourtvideo@gmail.com>
2022-08-19 09:29:22 -0400

    Updates monster options for more more specific queries

---
+++ Model/MonsterOptions.cs
@@ -0,0 +1,33 @@
+﻿namespace WumbosDnDToolbox.Model
+{
+    public class MonsterOptions
+    {
+        public enum Size
+        {
+            Tiny,
+            Small,
+            Medium,
+            Large,
+            Huge,
+            Gargantuan
+        }
+
+        public enum Type
+        {
+            Aberration,
+            Beast,
+            Celestial,
+            Construct,
+            Dragon,
+            Elemental,
+            Fey,
+            Fiend,
+            Giant,
+            Humanoid,
+            Monstrosity,
+            Ooze,
+            Plant,
+            Undead
+        }
+    }
+}
