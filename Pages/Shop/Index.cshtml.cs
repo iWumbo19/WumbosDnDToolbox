7f04ec265a6f0581cd4ee066895cc92f8d830110
Chris Rancourt <rancourtvideo@gmail.com>
2022-09-07 15:38:20 -0400

    Adds methods to convert form requests to needed information

--- Pages/Shop/Index.cshtml.cs
+++ Pages/Shop/Index.cshtml.cs
@@ -12,5 +12,58 @@ namespace WumbosDnDToolbox.Pages.Shop
         {
             Shop = new MagicShop(20);
         }
+
+        public void OnPostSearch()
+        {
+            Shop = new MagicShop(new MagicShopOptions(
+                Request.Form["levelFilter"],
+                Request.Form["typeFilter"],
+                Request.Form["sizeFilter"]));
+        }
+        public enum PlayerLevel
+        {
+            One = 1,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Eleven,
+            Twelve,
+            Thirteen,
+            Fourteen,
+            Fifteen,
+            Sixteen,
+            Seventeen,
+            Eighteen,
+            Nineteen,
+            Twenty
+        }
+        private int FormToLevel(string level)
+        {
+            switch (level)
+            {
+                case "One":
+                    return 1;
+                case "Two":
+                    return 2;
+                case "Three":
+                    return 3;
+                default:
+                    break;
+            }
+        }
+        private ShopType FormToType(string type)
+        {
+
+        }
+        private ShopSize FormToSize(string size)
+        {
+
+        }
     }
 }
